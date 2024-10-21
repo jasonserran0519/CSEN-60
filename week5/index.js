da48fffa11b28d11602990a0c3ee3e128df67b8f
jasonserran0519 <146367145+jasonserran0519@users.noreply.github.com>
2024-10-21 11:32:24 -0700

    wrote some js for week 5

---
+++ week5/index.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+//alert("hello world!");
+document.write('<p>Welcome to CSEN 60!</p>');
+
+// document,append('<p> This should come after the h1</p>')
+
+/*
+  @description this function adds two numbers
+  @parameter a 
+  @parameter b
+*/
+//TODO make s 
+function add(a, b) {
+    return a + b;
+}
