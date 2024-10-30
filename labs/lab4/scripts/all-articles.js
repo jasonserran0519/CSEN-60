a4434fc560741e3454a20e3b0a282ebc03254b10
jasonserran0519 <146367145+jasonserran0519@users.noreply.github.com>
2024-10-29 17:49:31 -0700

    finished lab4

--- labs/lab4/scripts/all-articles.js
+++ labs/lab4/scripts/all-articles.js
@@ -7,6 +7,9 @@ const tagLists = Array.from(document.querySelectorAll("article .tags"));
 
 // Search Functions
 
+/*
+funtion used to grab the search parameter for the tag. This allows for the search to occur 
+*/
 function initializeSearch(newParentElement) {
   const params = new URLSearchParams(window.location.search);
   if (newParentElement === null) {
@@ -23,7 +26,10 @@ function initializeSearch(newParentElement) {
   }
 }
 
-
+/* 
+hideArticles is used to hide articles that do not have the
+searched tag
+*/
 function hideArticles() {
   if (searchTags.length === 0) {
     for (const article of document.querySelectorAll("article")) {
@@ -44,10 +50,18 @@ function hideArticles() {
    * then add "hidden" to that article's classList
    * else, remove "hidden" from that article's classList
    */
-  // write your code here
+
+  for ( article of document.querySelectorAll("article")){
+    console.log(article)
+    if (articlesWithTags.includes(article)){
+      article.classList.remove("hidden");
+    }else{ 
+      (article.classList.add("hidden"));
+    }
+  }
 }
 
-/**
+/*
  * Creates a clickable tag button for a given search term (text). When clicked,
  * the button will remove the corresponding tag from both the DOM and the searchTags array.
  * This function also calls hideArticles to update the articles displayed after removal.
@@ -58,7 +72,11 @@ function createTag(text) {
    * add the class "tag" to its classList
    * set the button's textContent property to text (the passed in argument)
    */
-  // write your code here
+  
+
+  const button = document.createElement("button")
+  button.classList.add("tag")
+  button.textContent = text;
 
   function remove() {
     button.remove();
@@ -66,7 +84,6 @@ function createTag(text) {
     if (index !== -1) {
       searchTags.splice(index, 1);
     }
-
     hideArticles();
   }
 
@@ -74,9 +91,15 @@ function createTag(text) {
    * add a click event listener to the button, and set the listener to the remove function.
    * return the button element 
    */
-  // write your code here
+
+  button.addEventListener("click", remove)
+  return button;
+
 }
 
+/* 
+Function is called if a certain tag is inputted
+*/
 function findArticlesWithTag(phrase) {
   const articles = [];
   const sanitizedPhrase = phrase.toLowerCase().trim();
@@ -94,14 +117,24 @@ function findArticlesWithTag(phrase) {
 }
 
 
+/* 
+Creates a "tags" based on the text used as an input in the Website.
+This is then pushed to a list of "tags" that have been "searched"
+It later calls the hideArticles to hide the articles that do not match
+the searched tags. 
+*/ 
 function addSearchTerm(text) {
   parentElement.appendChild(createTag(text));
   searchTags.push(text);
   hideArticles();
 }
 
-// Handlers
 
+/*
+Function occurs when the user is trying to input a text in the website. 
+It handles the enter feature, passing the input value to 
+addSearchTerm
+*/
 function onSearch(event) {
   const input = event.currentTarget;
   /**
@@ -110,13 +143,19 @@ function onSearch(event) {
    * set input value to an empty string
    */
   // write your code here
+  if (event.key == "Enter"){
+    addSearchTerm(input.value)
+    input.value = ''
+  }
 }
 
 // Main function
+// Forever running while in the site
+//It is always listining for a keypress, passing that to 
+//onSearch
 
 function main() {
   initializeSearch(document.querySelector("#searched-tags"));
-
   document
     .querySelector("input[type=search]")
     .addEventListener("keypress", onSearch);
@@ -127,7 +166,7 @@ main();
 
 /**
  * Order of execution for each event:
- * Pressing Enter: 
- * Clicking to Remove a Tag: 
- * Loading the Page: 
+ * Pressing Enter: onSearch()-> addSearchTerm() -> hideArticles() 
+ * Clicking to Remove a Tag: onSearch() -->  hideArticles() --> remove()
+ * Loading the Page: main() -> initializeSearch() -> addSearchTerm() -> hideArticles()
  */
